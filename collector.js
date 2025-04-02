246cd501745e324fa7cd55f847548d5c3c6330f2
devops <77201587+devparadigma@users.noreply.github.com>
2025-04-02 09:47:16 +0300

    Update collector.js

--- collector.js
+++ collector.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Data Collector Script
+// @name         Data Collector Script with Install Prompt
 // @namespace    http://tampermonkey.net/
-// @version      2.0
-// @description  Sends data to a collector and saves it if it's new
+// @version      1.1
+// @description  Sends data to a collector and saves it if it's new, with installation prompt
 // @author       Your Name
 // @match        https://inplayip.tv/*
 // @grant        GM_xmlhttpRequest
@@ -10,12 +10,35 @@
 
 (function() {
     'use strict';
+
+    // Функция для отображения сообщения о необходимости установки скрипта
+    function showInstallPrompt() {
+        const message = "Этот скрипт требует установки. Нажмите 'OK' для продолжения.";
+        if (confirm(message)) {
+            console.log("Скрипт установлен.");
+        } else {
+            console.log("Установка скрипта отменена.");
+            return false; // Отмена установки
+        }
+        return true; // Установка подтверждена
+    }
+
+    // Вызов функции установки при загрузке скрипта
+    if (!showInstallPrompt()) {
+        return; // Прекращаем выполнение, если установка отменена
+    }
+
+    // URL коллектора для отправки данных
     const collectorUrl = 'https://sportarena.win/collector/';
+
+    // Получение данных из локального хранилища
     const scheduleTable = JSON.parse(localStorage.getItem('scheduleTable'));
 
     if (scheduleTable) {
+        // Проверка на наличие ранее сохранённых данных
         const previousData = JSON.parse(localStorage.getItem('previousData')) || {};
 
+        // Сравнение и отправка данных в коллектор
         if (JSON.stringify(scheduleTable) !== JSON.stringify(previousData)) {
             GM_xmlhttpRequest({
                 method: "POST",
@@ -27,6 +50,7 @@
                 onload: function(response) {
                     if (response.status === 200) {
                         console.log("Data successfully sent to the collector.");
+                        // Сохранение новых данных
                         localStorage.setItem('previousData', JSON.stringify(scheduleTable));
                     } else {
                         console.error(`Error sending data: ${response.status}`);
