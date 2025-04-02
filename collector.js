29c03a2b369b40c72d73796ab63de12acc137e67
devops <77201587+devparadigma@users.noreply.github.com>
2025-04-02 09:43:16 +0300

    Create collector.js

---
+++ collector.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Data Collector Script
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Sends data to a collector and saves it if it's new
+// @author       Your Name
+// @match        https://inplayip.tv/*
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const collectorUrl = 'https://sportarena.win/collector/';
+    const scheduleTable = JSON.parse(localStorage.getItem('scheduleTable'));
+
+    if (scheduleTable) {
+        const previousData = JSON.parse(localStorage.getItem('previousData')) || {};
+
+        if (JSON.stringify(scheduleTable) !== JSON.stringify(previousData)) {
+            GM_xmlhttpRequest({
+                method: "POST",
+                url: collectorUrl,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: JSON.stringify(scheduleTable),
+                onload: function(response) {
+                    if (response.status === 200) {
+                        console.log("Data successfully sent to the collector.");
+                        localStorage.setItem('previousData', JSON.stringify(scheduleTable));
+                    } else {
+                        console.error(`Error sending data: ${response.status}`);
+                    }
+                }
+            });
+        } else {
+            console.log("No new data to send.");
+        }
+    } else {
+        console.error("No schedule table found in local storage.");
+    }
+})();
